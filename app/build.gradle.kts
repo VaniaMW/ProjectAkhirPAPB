df33759df417c9aaff110c27f365785e52df2507
bona1507 <bnvt15@student.ub.ac.id>
2023-11-16 15:39:15 +0700

    haha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.stokbat"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.stokbat"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/stokbat/LoginActivity.java
+++ app/src/main/java/com/example/stokbat/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.stokbat;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.UnderlineSpan;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +13,24 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        TextView registerTextView = findViewById(R.id.register);
+        TextView tcTextView = findViewById(R.id.tc);
+
+        SpannableString span1 = new SpannableString(registerTextView.getText());
+        SpannableString span2 = new SpannableString(tcTextView.getText());
+
+        ForegroundColorSpan yellowColorSpan = new ForegroundColorSpan(getResources().getColor(R.color.buttonn));
+        UnderlineSpan underlineSpan = new UnderlineSpan();
+
+        span1.setSpan(yellowColorSpan, 18, span1.length(), SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+        span2.setSpan(yellowColorSpan, 45, 65, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        span1.setSpan(underlineSpan, 18, span1.length(), SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+        span2.setSpan(underlineSpan, 45, 65, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        registerTextView.setText(span1);
+        tcTextView.setText(span2);
+
     }
 }
